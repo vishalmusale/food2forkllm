e219c11db8ce599a775a1892110e3a05c76f1b95
vishalmusale <vishal.m.musale@gmail.com>
2025-03-17 09:53:32 -0700

    Updated Recipe Detail UI

--- app/src/main/java/com/example/food2forkellm/ui/MainActivity.kt
+++ app/src/main/java/com/example/food2forkellm/ui/MainActivity.kt
@@ -2,15 +2,12 @@ package com.example.food2forkellm.ui
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModelProvider
 import com.example.food2forkellm.data.network.RetrofitInstance
 import com.example.food2forkellm.data.repository.RecipeRepository
--- app/src/main/java/com/example/food2forkellm/ui/RecipeDetailsScreen.kt
+++ app/src/main/java/com/example/food2forkellm/ui/RecipeDetailsScreen.kt
@@ -1,6 +1,10 @@
 package com.example.food2forkellm.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -8,6 +12,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -19,22 +24,34 @@ fun RecipeDetailsScreen(recipeDetails: RecipeDetails, onBackClick: () -> Unit) {
     Column(
         modifier = Modifier
             .padding(16.dp)
-            .verticalScroll(rememberScrollState())
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Button(onClick = onBackClick) {
-            Text(text = "Back to List")
+        Column {
+            Spacer(modifier = Modifier.padding(20.dp))
+
+            AsyncImage(
+                model = recipeDetails.imageUrl,
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.padding(20.dp))
+
+            Text(text = "Ingredients:", fontSize = 20.sp, modifier = Modifier.padding(vertical = 8.dp))
+            recipeDetails.ingredients.forEach { ingredient ->
+                Text(text = "- $ingredient", fontSize = 16.sp, modifier = Modifier.padding(vertical = 4.dp))
+            }
         }
 
-        AsyncImage(
-            model = recipeDetails.imageUrl,
-            contentDescription = null,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Text(text = "Publisher: ${recipeDetails.publisher}", fontSize = 16.sp, modifier = Modifier.padding(vertical = 8.dp))
-        Text(text = "Ingredients:", fontSize = 20.sp, modifier = Modifier.padding(vertical = 8.dp))
-        recipeDetails.ingredients.forEach { ingredient ->
-            Text(text = "- $ingredient", fontSize = 16.sp, modifier = Modifier.padding(vertical = 4.dp))
+        Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+            Button(
+                onClick = onBackClick,
+                modifier = Modifier.align(Alignment.CenterVertically)
+            ) {
+                Text(text = "Back", fontSize = 22.sp, modifier = Modifier.padding(6.dp))
+            }
         }
     }
 }
